bc206f27e4585ebb16564f82fa2350a24adef9c1
adiljaz <146193187+adiljaz@users.noreply.github.com>
2024-01-26 10:55:16 +0530

    .

--- lib/Screens/Splash.dart
+++ lib/Screens/Splash.dart
@@ -1,95 +1,80 @@
-
-
 import 'dart:async';
 
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:galaxy/Screens/mianscreens/bodyHome.dart';
 
+
 import 'package:google_fonts/google_fonts.dart';
 import 'package:permission_handler/permission_handler.dart';
 
-
 class SplashScreen extends StatefulWidget {
-
-  const SplashScreen({super.key,});
- 
+  const SplashScreen({
+    super.key,
+  });
 
   @override
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-  
-
   @override
   void initState() {
-
     gotohome();
 
-requestPermission();
-  
+    requestPermission();
+
     super.initState();
   }
 
-
-void requestPermission() {
+  void requestPermission() {
     Permission.storage.request();
   }
-    
 
   @override
   Widget build(BuildContext context) {
-    MediaQueryData mediaQuerry=MediaQuery.of(context);
+    MediaQueryData mediaQuerry = MediaQuery.of(context);
 
-    return SafeArea (
+    return SafeArea(
       child: Scaffold(
-    
-        
         body: Stack(
-            children: [
-              Container(
-                color: const Color.fromARGB(255, 0, 0, 0),
-                width: mediaQuerry.size.width*2,
-                height: mediaQuerry.size.height*1,
-                child: Image.asset('assets/splash.jpeg',fit: BoxFit.cover),
-                
-        
-              ),
-               Positioned(
-                top: 280 ,
-                left: 110,
-                
-
-    
-                child:AnimatedTextKit(
+          children: [
+            Container(
+              color: const Color.fromARGB(255, 0, 0, 0),
+              width: mediaQuerry.size.width * 2,
+              height: mediaQuerry.size.height * 1,
+              child: Image.asset('assets/splash.jpeg', fit: BoxFit.cover),
+            ),
+            Positioned(
+              top: 280,
+              left: 110,
+              child: AnimatedTextKit(
                   repeatForever: true,
-
                   isRepeatingAnimation: true,
+                  animatedTexts: [
+                    ColorizeAnimatedText(' Galaxy ',
+                        textStyle: GoogleFonts.josefinSans(
+                            color: const Color.fromARGB(255, 190, 188, 188),
+                            fontSize: 35,
+                            fontWeight: FontWeight.bold),
+                        colors: [
+                          Colors.white,
+                          const Color.fromARGB(255, 65, 162, 242),
+                        ])
+                  ]),
+            ),
 
-                  
-                  animatedTexts:[ColorizeAnimatedText(' Galaxy ', textStyle: GoogleFonts.josefinSans( color: const Color.fromARGB(255, 190, 188, 188) ,fontSize: 35 ,fontWeight: FontWeight.bold ), colors: [
-                  Colors.white,
-                  const Color.fromARGB(255, 65, 162, 242) ,
-                  
-
-                ])]),
-                
-                ),
-
-                // Text('Galaxy',style:GoogleFonts.josefinSans( color: Color.fromARGB(255, 190, 188, 188) ,fontSize: 35 ,fontWeight: FontWeight.bold ),)
-            ],
-          ),
-      
+            // Text('Galaxy',style:GoogleFonts.josefinSans( color: Color.fromARGB(255, 190, 188, 188) ,fontSize: 35 ,fontWeight: FontWeight.bold ),)
+          ],
+        ),
       ),
     );
   }
 
   gotohome() async {
-   Timer(const Duration(seconds: 2), () { 
-     Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=>const Home()));
-   });
-   
+    Timer(const Duration(seconds: 2), () {
+      Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => const Home()));
+    });
   }
-
 }
--- lib/Screens/mianscreens/bodyHome.dart
+++ lib/Screens/mianscreens/bodyHome.dart
@@ -1,27 +1,21 @@
 // ignore: file_names
 import 'dart:developer';
-
 import 'package:animated_text_kit/animated_text_kit.dart';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_snake_navigationbar/flutter_snake_navigationbar.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:galaxy/Screens/mianscreens/Library.dart';
-
-import 'package:galaxy/Screens/home/home.dart';
-import 'package:galaxy/about.dart';
-import 'package:galaxy/colors/colors.dart';
-
-import 'package:galaxy/database/db_functions.dart';
-
 import 'package:galaxy/Screens/mianscreens/search.dart';
+
+import 'package:galaxy/about.dart';
+import 'package:galaxy/Screens/home/home.dart';
+import 'package:galaxy/colors/colors.dart';
+import 'package:galaxy/database/db_functions.dart';
 import 'package:galaxy/privacy.dart';
+
 import 'package:google_fonts/google_fonts.dart';
-
-
 import 'package:lottie/lottie.dart';
 import 'package:url_launcher/url_launcher.dart';
-
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -48,73 +42,155 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-
-        MediaQueryData mediaQuerry = MediaQuery.of(context);
+    MediaQueryData mediaQuerry = MediaQuery.of(context);
     getAllSongs();
-    
 
     return SafeArea(
-      
       child: Scaffold(
         extendBody: true,
         backgroundColor: Colors.transparent,
         key: Home.scaffoldKey,
         drawer: Drawer(
-        
           child: Container(
-            decoration: BoxDecoration(gradient: LinearGradient(colors: [
-              Colors.white,Color.fromARGB(255, 255, 255, 255)
+            decoration: BoxDecoration(
+                gradient: LinearGradient(colors: [
+              Colors.white,
+              Color.fromARGB(255, 255, 255, 255)
             ])),
             child: ListView(
               children: [
-            
-              SizedBox(
-                height: mediaQuerry.size.height*0.1 ,
-              ),
-            
-                Center( child: Lottie.asset('assets/drawer.json')),
-                 SizedBox(
-                height: mediaQuerry.size.height*0.01 ,
-              ),
-            
-                Center(
-                  child: AnimatedTextKit(
-                     
-                  
-                     repeatForever: false,
-                  
-                      
-                      animatedTexts:[ColorizeAnimatedText(' Galaxy', textStyle: GoogleFonts.josefinSans( color: const Color.fromARGB(255, 190, 188, 188) ,fontSize: 50 ,fontWeight: FontWeight.bold ), colors: [
-                      Color.fromARGB(255, 0, 0, 0),
-                      Color.fromARGB(255, 255, 255, 255) ,
-                      
-                  
-                    ])]),
+                SizedBox(
+                  height: mediaQuerry.size.height * 0.1,
                 ),
-            
-                SizedBox(height: mediaQuerry.size.height*0.07,),
-            
-            
-                GestureDetector(   onTap: (){},  child: Row(     children:[     SizedBox(width: mediaQuerry.size.width*0.07 ,),    FaIcon(FontAwesomeIcons.shareNodes,size: 30,)  ,SizedBox(width: mediaQuerry.size.width*0.07 ,),Text('Share',style:GoogleFonts.lato(fontWeight: FontWeight.bold ,fontSize: 27 ),)    ],)),
-                SizedBox(height:mediaQuerry.size.height*0.02,),
-            
-                     GestureDetector(       onTap: (){
-                      Navigator.of(context).push(MaterialPageRoute(builder: (context)=>Privacy()));
-                     },      child: Row(     children:[     SizedBox(width: mediaQuerry.size.width*0.06 ,),    Icon(Icons.security,size: 30,),SizedBox(width: mediaQuerry.size.width*0.07 ,),Text('Privacy Policy',style:GoogleFonts.lato(fontWeight: FontWeight.bold ,fontSize: 27 ),)    ],)),
-                 SizedBox(height:mediaQuerry.size.height*0.02,),
-            
-                     GestureDetector(
-                      onTap: (){
-                        _launchEmail();
-                      },
-                      child: Row(     children:[     SizedBox(width: mediaQuerry.size.width*0.06 ,),    Icon(Icons.forum,size: 30,),SizedBox(width: mediaQuerry.size.width*0.07 ,),Text('FeedBack',style:GoogleFonts.lato(fontWeight: FontWeight.bold ,fontSize: 27 ),)    ],)),
-                     SizedBox(height:mediaQuerry.size.height*0.02,),
-            
-                     GestureDetector(         onTap: (){
-                      Navigator.of(context).push(MaterialPageRoute(builder: (context)=>About()));
-                     },  child: Row(     children:[     SizedBox(width: mediaQuerry.size.width*0.06 ,),    Icon(Icons.report,size: 30,),SizedBox(width: mediaQuerry.size.width*0.07 ,),Text('About',style:GoogleFonts.lato(fontWeight: FontWeight.bold ,fontSize: 27 ),)    ],)),
-                          SizedBox(height: mediaQuerry.size.height*0.16,),
-                     Center(child: Text('V.1.0.0',style: TextStyle(fontSize: 15,fontWeight: FontWeight.w500 ),))
+                Center(child: Lottie.asset('assets/drawer.json')),
+                SizedBox(
+                  height: mediaQuerry.size.height * 0.01,
+                ),
+                Center(
+                  child: AnimatedTextKit(repeatForever: false, animatedTexts: [
+                    ColorizeAnimatedText(' Galaxy',
+                        textStyle: GoogleFonts.josefinSans(
+                            color: const Color.fromARGB(255, 190, 188, 188),
+                            fontSize: 50,
+                            fontWeight: FontWeight.bold),
+                        colors: [
+                          Color.fromARGB(255, 0, 0, 0),
+                          Color.fromARGB(255, 255, 255, 255),
+                        ])
+                  ]),
+                ),
+                SizedBox(
+                  height: mediaQuerry.size.height * 0.07,
+                ),
+                GestureDetector(
+                    onTap: () {},
+                    child: Row(
+                      children: [
+                        SizedBox(
+                          width: mediaQuerry.size.width * 0.07,
+                        ),
+                        FaIcon(
+                          FontAwesomeIcons.shareNodes,
+                          size: 30,
+                        ),
+                        SizedBox(
+                          width: mediaQuerry.size.width * 0.07,
+                        ),
+                        Text(
+                          'Share',
+                          style: GoogleFonts.lato(
+                              fontWeight: FontWeight.bold, fontSize: 27),
+                        )
+                      ],
+                    )),
+                SizedBox(
+                  height: mediaQuerry.size.height * 0.02,
+                ),
+                GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).push(
+                          MaterialPageRoute(builder: (context) => Privacy()));
+                    },
+                    child: Row(
+                      children: [
+                        SizedBox(
+                          width: mediaQuerry.size.width * 0.06,
+                        ),
+                        Icon(
+                          Icons.security,
+                          size: 30,
+                        ),
+                        SizedBox(
+                          width: mediaQuerry.size.width * 0.07,
+                        ),
+                        Text(
+                          'Privacy Policy',
+                          style: GoogleFonts.lato(
+                              fontWeight: FontWeight.bold, fontSize: 27),
+                        )
+                      ],
+                    )),
+                SizedBox(
+                  height: mediaQuerry.size.height * 0.02,
+                ),
+                GestureDetector(
+                    onTap: () {
+                      _launchEmail();
+                    },
+                    child: Row(
+                      children: [
+                        SizedBox(
+                          width: mediaQuerry.size.width * 0.06,
+                        ),
+                        Icon(
+                          Icons.forum,
+                          size: 30,
+                        ),
+                        SizedBox(
+                          width: mediaQuerry.size.width * 0.07,
+                        ),
+                        Text(
+                          'FeedBack',
+                          style: GoogleFonts.lato(
+                              fontWeight: FontWeight.bold, fontSize: 27),
+                        )
+                      ],
+                    )),
+                SizedBox(
+                  height: mediaQuerry.size.height * 0.02,
+                ),
+                GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).push(
+                          MaterialPageRoute(builder: (context) => About()));
+                    },
+                    child: Row(
+                      children: [
+                        SizedBox(
+                          width: mediaQuerry.size.width * 0.06,
+                        ),
+                        Icon(
+                          Icons.report,
+                          size: 30,
+                        ),
+                        SizedBox(
+                          width: mediaQuerry.size.width * 0.07,
+                        ),
+                        Text(
+                          'About',
+                          style: GoogleFonts.lato(
+                              fontWeight: FontWeight.bold, fontSize: 27),
+                        )
+                      ],
+                    )),
+                SizedBox(
+                  height: mediaQuerry.size.height * 0.16,
+                ),
+                Center(
+                    child: Text(
+                  'V.1.0.0',
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500),
+                ))
               ],
             ),
           ),
@@ -262,54 +338,75 @@ class _HomeState extends State<Home> {
                     topLeft: Radius.circular(40),
                     topRight: Radius.circular(40),
                   ),
-                  child: SnakeNavigationBar.color( 
+                  child: SnakeNavigationBar.color(
                     height: 70, snakeViewColor: Colormanager.listtile,
 
-                     // Set to the desired color
+                    // Set to the desired color
                     backgroundColor: Colors.black,
-                    currentIndex : _selectedindex,
+                    currentIndex: _selectedindex,
                     onTap: (value) {
                       setState(() {
                         _selectedindex = value;
                       });
                     },
-                    items:const  [
-                      BottomNavigationBarItem(icon: Center(
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            Icon(
-                              Icons.home,
-                              color: Colors.white,
-                            ),
-                                Text('Home',style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white),)
-                          ],
+                    items: const [
+                      BottomNavigationBarItem(
+                        icon: Center(
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Icon(
+                                Icons.home,
+                                color: Colors.white,
+                              ),
+                              Text(
+                                'Home',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white),
+                              )
+                            ],
+                          ),
                         ),
-                      ),),
-                      BottomNavigationBarItem(icon: Center(
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            Icon(
-                              Icons.search,
-                              color: Colors.white,
-                            ),
-                            Text('Search',style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white),)
-                          ],
+                      ),
+                      BottomNavigationBarItem(
+                        icon: Center(
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Icon(
+                                Icons.search,
+                                color: Colors.white,
+                              ),
+                              Text(
+                                'Search',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white),
+                              )
+                            ],
+                          ),
                         ),
-                      ),),
-                      BottomNavigationBarItem(icon: Center(
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            Icon(
-                              Icons.library_music,
-                              color: Colors.white,
-                            ),
-                                Text('Library',style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white),)
-                          ],
+                      ),
+                      BottomNavigationBarItem(
+                        icon: Center(
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Icon(
+                                Icons.library_music,
+                                color: Colors.white,
+                              ),
+                              Text(
+                                'Library',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white),
+                              )
+                            ],
+                          ),
                         ),
-                      ),),
+                      ),
                     ],
                   ))
             ],
@@ -319,8 +416,7 @@ class _HomeState extends State<Home> {
     );
   }
 
-
- Future<void> _launchEmail() async {
+  Future<void> _launchEmail() async {
     const String emailAddress = 'adiljaz17@gmail.com';
     const String emailSubject = '';
     const String emailBody = '';
@@ -337,11 +433,9 @@ class _HomeState extends State<Home> {
     try {
       await launchUrl(emailUri);
     } catch (e) {
-      
       log('Error launching email: $e');
     }
   }
-
 }
 
 
